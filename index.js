5ba5ce2612ea2502502951141c8fe912bbdbf753
hsingjouisme <161863040+hsingjouisme@users.noreply.github.com>
2024-03-03 17:53:49 +0800

    20240303-2

--- index.js
+++ index.js
@@ -32,37 +32,3 @@ text.innerHTML=text.innerText.split("").map(
     `<span style="transform:rotate(${i*12}deg)">${char}</span> `   //讓circle_text上的文字都分別在一個<span>中
 ).join("")
 
-
-//作品按按鈕切換
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("gallery");
-  if (n > slides.length) {
-    slideIndex = 1
-}    
-  if (n < 1) {
-    slideIndex = slides.length
-}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slides[slideIndex-1].style.display = "grid";  
-}
-
-function open_layout_1(){
-    document.getElementById('learning_vue').style.display='block';
-    document.getElementById('vue_more_icon').style.display='none';
-    document.getElementById('vue_less_icon').style.display="block";
-}
-function open_layout_2(){
-    document.getElementById('learning_git').style.display='block';
-    document.getElementById('git_more_icon').style.display='none';
-    document.getElementById('git_less_icon').style.display="block";
-}
